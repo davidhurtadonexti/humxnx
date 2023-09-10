3ae01622127806a48ca7e1e55945a8d0e2f42ec1
David Hurtado <david.hurtado.chichande@gmail.com>
2023-09-10 18:12:29 -0500

    add blob storage

--- Humxnx.Core/Services/Historial/src/Historial.Functions/HandlerEventGrid.cs
+++ Humxnx.Core/Services/Historial/src/Historial.Functions/HandlerEventGrid.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.EventGrid;
 using Humxnx.Historial.Core.Domain.Entities;
@@ -13,11 +15,16 @@ using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 public static class ReactiveApiFunction
 {
     // Utilizaremos un BehaviorSubject para mantener un flujo de eventos simulados
-    private static readonly Dictionary<string, Subject<string>> sessionEventStreams = new Dictionary<string, Subject<string>>();
+    private static readonly Dictionary<string, Subject<string>> SessionEventStreams = new Dictionary<string, Subject<string>>();
 
 
     [FunctionName("ObservableEventGrid")]
@@ -30,38 +37,51 @@ public static class ReactiveApiFunction
         // Establece la respuesta HTTP como un flujo de eventos (Content-Type: text/event-stream)
         var response = req.HttpContext.Response;
         response.Headers.Add("Content-Type", "text/event-stream");
+        response.Headers.Add("Cache-Control", "no-cache");
+        response.Headers.Add("Connection", "keep-alive");
         
+   
+        // Obtener el estado de la sesión desde Azure Blob Storage
+        var sessionStateId = await GetSessionStateFromStorage(sessionId, log);
         // Verifica si ya existe un flujo de eventos para esta sesión
-        var checkSession = sessionEventStreams.TryGetValue(sessionId, out var ChekckEventObserver);
+        var checkSession = SessionEventStreams.TryGetValue(sessionStateId, out var chekckEventObserver);
         if (!checkSession)
         {
           
             // Si no existe, crea uno nuevo para esta sesión
             var newEventStream = new Subject<string>();
-            ChekckEventObserver = newEventStream;
+            chekckEventObserver = newEventStream;
 
             // Agrega el nuevo flujo de eventos al diccionario
-            sessionEventStreams.Add(sessionId, ChekckEventObserver);
-            log.LogInformation("Init session: " + sessionId);
-            var messageData = new { data = "Stream creado exitosamente con SessionID: "+ sessionId};
+            SessionEventStreams.Add(sessionStateId, chekckEventObserver);
+            
+            // Genera un ID de sesión único (puedes usar cualquier lógica de generación que necesites)
+            // string newSessionId = Guid.NewGuid().ToString();
+
+            // El estado de sesión inicial
+
+            // Guarda el estado de sesión en Azure Blob Storage
+            await SaveSessionStateToStorage(sessionStateId, sessionStateId, log);
+
+            log.LogInformation("Init session: " + sessionStateId);
+            var messageData = new { data = "Stream creado exitosamente con SessionID: "+ sessionStateId};
             await response.WriteAsync($"data: {messageData}\n\n");
             await response.Body.FlushAsync();
-            log.LogInformation("Async response sessionId: " + sessionId);
             // Elimina el flujo de eventos de la sesión cuando se cierra la conexión
             response.OnCompleted(() =>
             {
-                sessionEventStreams.Remove(sessionId);
+                SessionEventStreams.Remove(sessionStateId);
                 return Task.CompletedTask;
             });
-            log.LogInformation("Correctt flow SessionId: " + sessionId);
-            // return new OkResult();
+            log.LogInformation("Successfully SessionId: " + sessionStateId);
         }
         
     
 
         try
         {
-            if (sessionEventStreams.TryGetValue(sessionId, out var eventObserver))
+            log.LogInformation($"data: {sessionStateId}\n\n");
+            if (SessionEventStreams.TryGetValue(sessionStateId, out var eventObserver))
             {
                 // Suscríbete al flujo de eventos y envía eventos al cliente
                 async void OnNext(string eventData)
@@ -70,11 +90,12 @@ public static class ReactiveApiFunction
 
                     if (eventData != null)
                     {
-                        log.LogInformation("Se levanta el Observador para la SessionID: " + sessionId);
+                        log.LogInformation("Se levanta el Observador para la SessionID: " + sessionStateId);
                         var messageData = new { data = eventData };
                         var eventDataJsons = JsonSerializer.Serialize(messageData);
                         await response.WriteAsync($"data: {eventDataJsons}\n\n");
                         await response.Body.FlushAsync();
+                        log.LogInformation("Escribiendo mensaje para la SessionId; : " + sessionStateId);
                     }
                 }
 
@@ -101,7 +122,7 @@ public static class ReactiveApiFunction
             QueueMessage messageReceived = JsonSerializer.Deserialize<QueueMessage>(message);
             logger.LogInformation($"Se captura el mensaje del EventGrid por medio del Service bus: {message}");
          
-            if (sessionEventStreams.TryGetValue(messageReceived.SessionId, out var eventObserver))
+            if (SessionEventStreams.TryGetValue(messageReceived.SessionId, out var eventObserver))
             {
                 logger.LogInformation("response SessionId: " + messageReceived.SessionId);
                  eventObserver.OnNext(message);
@@ -110,6 +131,76 @@ public static class ReactiveApiFunction
         catch (Exception ex)
         {
             Console.WriteLine($"Error al recibir mensajes: {ex.Message}");
+        }
+    }
+    
+    private static async Task SaveSessionStateToStorage(string sessionId, string sessionState, ILogger log)
+    {
+        string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        string containerName = "session-container-test"; // Nombre del contenedor en Azure Blob Storage
+
+        try
+        {
+            // Crear una instancia del cliente de Blob Storage
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            // Obtener una referencia al contenedor
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Obtener una referencia al blob que contendrá el estado de la sesión
+            BlobClient blobClient = containerClient.GetBlobClient($"{sessionId}.json");
+
+            // Convertir el estado de sesión en bytes
+            byte[] sessionStateBytes = Encoding.UTF8.GetBytes(sessionState);
+
+            // Cargar los bytes en el blob
+            using (MemoryStream stream = new MemoryStream(sessionStateBytes))
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+        }
+        catch (RequestFailedException ex)
+        {
+            log.LogError($"Error al acceder a Azure Blob Storage: {ex.Message}");
+            throw; // Manejar adecuadamente el error según tus necesidades
+        }
+    }
+    
+    private static async Task<string> GetSessionStateFromStorage(string sessionId, ILogger log)
+    {
+        string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        string containerName = "session-container-test"; 
+        try
+        {
+            // Crear una instancia del cliente de Blob Storage
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            // Obtener una referencia al contenedor
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Obtener una referencia al blob que contiene el estado de la sesión
+            BlobClient blobClient = containerClient.GetBlobClient($"{sessionId}.json");
+
+            // Verificar si el blob existe
+            if (await blobClient.ExistsAsync())
+            {
+                // Descargar el contenido del blob
+                BlobDownloadInfo blobDownloadInfo = await blobClient.DownloadAsync();
+
+                // Leer el contenido y devolverlo como string
+                using (StreamReader reader = new StreamReader(blobDownloadInfo.Content))
+                {
+                    return await reader.ReadToEndAsync();
+                }
+            }
+
+            // Si el blob no existe, puedes devolver un estado de sesión inicial
+            return  sessionId;
+        }
+        catch (RequestFailedException ex)
+        {
+            log.LogError($"Error al acceder a Azure Blob Storage: {ex.Message}");
+            throw; // Manejar adecuadamente el error según tus necesidades
         }
     }
 }
