c6bb2bf8ae951a86ff3fcf387a5a90a2be711777
David Hurtado <david.hurtado.chichande@gmail.com>
2023-09-12 18:23:04 -0500

    add await flush

--- Humxnx.Core/Services/Historial/src/Historial.Functions/HandlerEventGrid.cs
+++ Humxnx.Core/Services/Historial/src/Historial.Functions/HandlerEventGrid.cs
@@ -38,9 +38,9 @@ public static class HandlerEventGrid
         var messageData = new { data = "Stream creado exitosamente"};
         var eventDataJsons = JsonSerializer.Serialize(messageData);
         // Escribe los datos JSON seguidos de "\r\n"
-        await response.WriteAsync($"{eventDataJsons}\r\n");
-        await response.WriteAsync($"{eventDataJsons}\n\n");
-        
+        await response.WriteAsync($"data: {eventDataJsons}\n\n", Encoding.UTF8);
+        await response.Body.FlushAsync();
+        await response.Body.FlushAsync();
         await response.Body.FlushAsync();
 
         // Suscríbete al Observable y envía eventos SSE al cliente
@@ -49,8 +49,11 @@ public static class HandlerEventGrid
             var messageData = new { data = sseEvent };
             var eventDataJsons = JsonSerializer.Serialize(messageData);
             // Envía el evento SSE al cliente de forma asincrónica
-            await response.WriteAsync($"{eventDataJsons}\r\n");
             await response.WriteAsync($"data: {eventDataJsons}\n\n", Encoding.UTF8);
+            await response.Body.FlushAsync();
+            await response.Body.FlushAsync();
+            await response.Body.FlushAsync();
+            await response.Body.FlushAsync();
             await response.Body.FlushAsync();
         });
 
